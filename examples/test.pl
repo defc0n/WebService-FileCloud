f2deba39a5bc053cc6ab3fdd3430e29dcb533909
Mitch McCracken <mrmccrac@gmail.com>
2013-09-22 21:52:23 -0400

    importing

---
+++ examples/test.pl
@@ -0,0 +1,28 @@
+#!perl
+
+use strict;
+use warnings;
+
+use WebService::FileCloud;
+use Data::Dumper;
+
+# update these values accordingly
+use constant AKEY => 'APIKEY';
+use constant UKEY => 'FILEKEY';
+use constant TKEY => 'TAGKEY';
+use constant USERNAME => 'USERNAME';
+use constant PASSWORD => 'PASSWORD';
+use constant UPLOAD_URL => 'URL';
+use constant FILENAME => 'FILENAME';
+
+my $websvc = WebService::FileCloud->new( akey => AKEY );
+
+warn( Dumper( $websvc->fetch_apikey( username => USERNAME, password => PASSWORD ) ) );
+warn( Dumper( $websvc->fetch_account_details() ) );
+warn( Dumper( $websvc->ping() ) );
+warn( Dumper( $websvc->check_file( ukey => UKEY ) ) );
+warn( Dumper( $websvc->fetch_file_details( ukey => UKEY ) ) );
+warn( Dumper( $websvc->fetch_download_url( ukey => UKEY ) ) );
+warn( Dumper( $websvc->fetch_tag_details( tkey => TKEY ) ) );
+warn( Dumper( $websvc->fetch_upload_url() ) );
+warn( Dumper( $websvc->upload_file( url => UPLOAD_URL, filename => FILENAME ) ) );
